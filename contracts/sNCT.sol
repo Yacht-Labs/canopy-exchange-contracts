2beba9dd05c44d594125d89edbfcb01c87f35ddf
Henry Minden <henry.minden@bluelabellabs.com>
2022-03-28 18:26:12 -0500

    added mintWithEvent function

--- contracts/sNCT.sol
+++ contracts/sNCT.sol
@@ -9,11 +9,19 @@ contract sNCT is ERC20PresetMinterPauser {
 
     event Burn(uint256 amount, address from);
 
+    event Mint(uint256 amount, address to, uint256 txHash);
+
     constructor(string memory name, string memory symbol) ERC20PresetMinterPauser (name, symbol) {}
 
     function burn(uint256 amount) override public {
         ERC20Burnable.burn(amount);
         emit Burn(amount, msg.sender);
     }
+
+    function mintWithEvent(address to, uint256 amount, uint256 txHash) public {
+        require(hasRole(MINTER_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have minter role to mint");
+        ERC20PresetMinterPauser.mint(to, amount);
+        emit Mint(amount, to, txHash);
+    } 
 
 }
